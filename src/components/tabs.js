70e1314c5577c50a4fec1007104448515fc273b2
Adalberto Sampaio <sampaio.afn@gmail.com>
2020-09-15 21:31:12 -0300

    Corrected error

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -44,7 +44,7 @@ const Slider = styled(motion.div)`
   background: rgba(249, 191, 58, 1);
 `;
 
-const tabs = [ContentTabs.tab1, "Motion", "Vídeo", "Filtro", "Fotografia", "Site/Banner", "E-mail MKT", "E-books/Infográficos"]
+const tabs = ["Redes Sociais", "Motion", "Vídeo", "Filtro", "Fotografia", "Site/Banner", "E-mail MKT", "E-books/Infográficos"]
 
 function Tabs() {
   const [value, setValue] = useState(1)
@@ -114,8 +114,8 @@ function Tabs() {
           key="{tab}"
         >
           <ContentBox
-            title={ContentTabs.tab1.title}
-            content={ContentTabs.tab1.text}
+            title="Redes Sociais"
+            content="Redes Sociais"
           />
         </div>
         <div
