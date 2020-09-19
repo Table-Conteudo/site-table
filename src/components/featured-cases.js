bef199abd8b6da413c656e285c9444c49461f018
Adalberto Sampaio <sampaio.afn@gmail.com>
2020-09-18 22:32:37 -0300

    Special case hover

--- src/components/featured-cases.js
+++ src/components/featured-cases.js
@@ -1,14 +1,19 @@
 import React from "react"
 import caseStyles from "./cases.module.scss"
+import { motion } from "framer-motion"
 
 export default function FeaturedCases({ children }) {
   return (
     <div className={caseStyles.container}>
-        <div className={caseStyles.rectangle}></div>
-        <div className={caseStyles.wrapper}>
-          <h1>Nossos cases</h1>
-          <div className={caseStyles.flex}>{children}</div>
-        </div>
+      <div className={caseStyles.rectangle}></div>
+      <div className={caseStyles.wrapper}>
+        <h1>Nossos cases</h1>
+        <motion.div
+          className={caseStyles.flex}
+        >
+          {children}
+        </motion.div>
+      </div>
     </div>
   )
 }
